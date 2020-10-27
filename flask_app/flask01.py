e6a85d61e3fff2a988364aff0859ad368209b1db
tnguy231-TN <71344519+tnguy231-TN@users.noreply.github.com>
2020-10-26 23:02:21 -0400

    flask app added templates and static

--- flask_app/flask01.py
+++ flask_app/flask01.py
@@ -19,7 +19,7 @@ def index():
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
-#   http://127.0.0.1:5000
+# http://127.0.0.1:5000
 
 # Note that we are running with "debug=True", so if you make changes and save it
 # the server will automatically update. This is great for development but is a
