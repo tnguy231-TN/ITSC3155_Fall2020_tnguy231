89342c2e5d3f734919aa6bd39dc710853c7bdf82
Thao Nguyen <tnguy231@uncc.edu>
2020-12-01 01:48:06 -0500

    adding and displaying comments functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -14,6 +14,9 @@ from models import Note as Note
 from models import User as User
 from forms import RegisterForm
 from forms import LoginForm
+from forms import CommentForm
+from models import Comment as Comment
+
 
 
 app = Flask(__name__)  # create an app
@@ -62,7 +65,10 @@ def get_note(note_id):
     if session.get('user'):
         my_note = db.session.query(Note).filter_by(id=note_id, user_id=session['user_id']).one()
 
-        return render_template('note.html', note=my_note, user=session['user'])
+        # create a comment form object
+        form = CommentForm()
+
+        return render_template('note.html', note=my_note, user=session['user'], form=form)
     else:
         return redirect(url_for('login'))
 
@@ -195,6 +201,23 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
